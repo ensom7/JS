b66f488ee51733274981dd9c4409821fc44aee7e
ensom7 <aliyashakirova745@gmail.com>
2023-09-07 15:46:29 +0300

    Обнов

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-alert('hello')
+let city = prompt('Название города');
+let year = prompt('Год образования');
+let people = prompt('Население');
+
+let thisyear = 2023;
+let l = thisyear - year;
+
+
+
+alert ('Городу ' + city + ' исполнилось ' + l + ' лет с момента его образования. Население - ' + people + ' человек');
